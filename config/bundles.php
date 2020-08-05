1d488d679ac6a2ab1a7e73616747791a1ab09efe
Ivan Avramov <itavramov@gmail.com>
2020-08-05 16:30:42 +0300

    remove more dev bundles

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
